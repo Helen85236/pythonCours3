2ebcf6e316fdb9365ead5727016d571566c183a4
Helen85236 <128638608+Helen85236@users.noreply.github.com>
2023-04-19 00:40:22 +0300

    add files

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils import get_data, filter_data, sort_data, format_data
+
+
+def main():
+    data = get_data()
+    data = filter_data(data)
+    data = sort_data(data)
+    data = format_data(data)
+
+    for row in data:
+        print(row)
+
+
+
+if __name__ == "__main__":
+    main()
